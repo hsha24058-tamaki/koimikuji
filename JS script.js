3bf0c1214e8a327d156578edbbe4ee93ecaa755c
tamaki <hsha24058@ict.shimanet.ed.jp>
2026-04-17 14:49:30 +0900

    初回コミット

---
+++ JS script.js
@@ -0,0 +1,16 @@
+const fortunes=[
+        "大吉　素敵な出会いがあるかも！",
+        "中吉　少し勇気を出すと良いことが起きる",
+        "小吉　今日はゆっくり過ごそう",
+        "凶　無理せず慎重に行動しよう"
+];
+
+const button = Document.getElementById("a");
+const result = document.getElementById("b");
+
+button.addEventListener("click", function () {
+    const randomNumber =Math.floor(Math.random()*fortunes.length);
+    
+    result.textContent = fortunes[(randomNumber)];
+
+})
